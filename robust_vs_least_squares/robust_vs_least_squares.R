6be7d0156a0fcfa9a1c6acaba0291a5b1f05ca61
Tim Lucas <timcdlucas@gmail.com>
2021-10-27 14:21:21 +0100

    Robust linear model comparison.

---
+++ robust_vs_least_squares/robust_vs_least_squares.R
@@ -0,0 +1,47 @@
+
+
+
+# Compare robust and normal linear regression.
+
+library(ggplot2)
+library(MASS)
+
+set.seed(11982)
+
+# Create data.
+N <- 30
+x <- runif(N)
+y <- x + rnorm(N, 0, 0.1)
+
+y[which.min(x)] <- y[which.min(x)] - 6
+
+plot(y ~ x)
+
+d <- data.frame(y, x)
+
+lm <- lm(y ~ x, d)
+lmt <- paste0('Least squares. beta = ', round(lm$coefficients[2], 2), '. (true value = 1)')
+
+ggplot(d, aes(x, y)) + 
+    geom_point() + 
+    geom_smooth(method = 'lm') + 
+    ggtitle(lmt) +
+    theme(text = element_text(size=27))
+ggsave('lm.png')
+
+
+rlm <- rlm(y ~ x, d)
+rlmt <- paste0('Least squares. beta = ', round(rlm$coefficients[2], 2), '. (true value = 1)')
+
+ggplot(d, aes(x, y)) + 
+    geom_point() + 
+    geom_smooth(method = 'rlm') + 
+    ggtitle(rlmt) +
+    theme(text = element_text(size=27))
+ggsave('rlm.png')
+
+
+
+
+
+
